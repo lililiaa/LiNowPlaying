5b8467be557e0a369c0ff972ae097bf91db2f9b4
lililiaa <2381835193@qq.com>
2025-10-30 20:41:05 +0800

    代理配置修改

--- src/utils/cover.js
+++ src/utils/cover.js
@@ -2,9 +2,9 @@ export const getCoverUrl = (url) => {
   if (!url) return '';
   // if (process.env.NODE_ENV === 'development') {
   if (url.includes('y.qq.com')) {
-    return url.replace('https://y.qq.com/', '/qq-proxy');
+    return url.replace('https://y.qq.com/', '/qq-proxy/');
   } else if (url.includes('p3.music.126.net')) {
-    return url.replace('http://p3.music.126.net/', '/netease-proxy');
+    return url.replace('http://p3.music.126.net/', '/netease-proxy/');
   }
   return url;
   // } else {
